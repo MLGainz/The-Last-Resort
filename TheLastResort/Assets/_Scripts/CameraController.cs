e9e4c29e381807e404ddb7b2776e7704be32e6d5
Sean Leonard <spleonard1@yahoo.com>
2017-10-31 16:37:02 -0400

    Mouse rotates camera and bow

--- TheLastResort/Assets/_Scripts/CameraController.cs
+++ TheLastResort/Assets/_Scripts/CameraController.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
+	
+	public float speedH = 2.0f;
+    public float speedV = 2.0f;
 
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+	private float oldpitch;
+	
     private Vector3 offset;
+	private Vector3 lookPos;
 
     void Start()
     {
@@ -14,7 +22,17 @@ public class CameraController : MonoBehaviour {
     }
 
     void LateUpdate()
-    {
+    {	
         transform.position = player.transform.position + offset;
+		yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+		
+		if(pitch < 90 && pitch > -90){
+			transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+			player.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+			oldpitch = pitch;
+		}else{
+			transform.eulerAngles = new Vector3(oldpitch, yaw, 0.0f);
+		}
     }
 }
