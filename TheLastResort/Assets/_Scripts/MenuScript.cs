0e7f6a2b27090a8aa6e578aa80cdba73fa064bf1
dfowler621 <dfowler621@gmail.com>
2017-11-02 09:46:32 -0400

    more menu work

---
+++ TheLastResort/Assets/_Scripts/MenuScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startHunterText;
+    public Button startDeerText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startHunterText = startHunterText.GetComponent<Button>();
+        startDeerText = startDeerText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+		
+	}
+	
+	public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startHunterText.enabled = false;
+        startDeerText.enabled = false;
+        exitText.enabled = false;
+
+
+    }
+	
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startHunterText.enabled = true;
+        startDeerText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartDeerLevel()
+    {
+        SceneManager.LoadScene("DeerScene");
+    }
+
+    public void StartHunterLevel()
+    {
+        SceneManager.LoadScene("HunterScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
