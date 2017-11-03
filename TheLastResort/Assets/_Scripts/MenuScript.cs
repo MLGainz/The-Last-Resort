0a65ad5f30ec672e3351587f0c2728d462d92e8d
dfowler621 <dfowler621@gmail.com>
2017-11-03 14:48:36 -0400

    Menu work

--- TheLastResort/Assets/_Scripts/MenuScript.cs
+++ TheLastResort/Assets/_Scripts/MenuScript.cs
@@ -7,27 +7,75 @@ using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour {
 
     public Canvas quitMenu;
+    public Canvas OfflineMenu;
+    public Canvas OnlineMenu;
     public Button startHunterText;
     public Button startDeerText;
     public Button exitText;
+    public Button startOnlineText;
+    public Button startOfflineText;
+    public Button GoBackText;
 
 	// Use this for initialization
 	void Start () {
 
         quitMenu = quitMenu.GetComponent<Canvas>();
-        startHunterText = startHunterText.GetComponent<Button>();
-        startDeerText = startDeerText.GetComponent<Button>();
+        startOnlineText = startOnlineText.GetComponent<Button>();
+        startOfflineText = startOfflineText.GetComponent<Button>();
+
+        //startHunterText = startHunterText.GetComponent<Button>();
+        //startDeerText = startDeerText.GetComponent<Button>();
         exitText = exitText.GetComponent<Button>();
+        //startHunterText.enabled = false;
+        //startDeerText.enabled = false;
+        OfflineMenu.enabled = false;
+        OnlineMenu.enabled = false;
         quitMenu.enabled = false;
+
 		
 	}
 	
-	public void ExitPress()
+    public void startDeerOnline()
+    {
+    //Start deer online
+    }
+
+    public void startHunterOnline()
+    {
+    //Start deer Offline
+    }
+
+    public void OnlinePress()
+    {
+        OnlineMenu.enabled = true;
+        startOnlineText.enabled = false;
+        startOfflineText.enabled = false;
+    }
+
+    public void OfflinePress()
+    {
+        OfflineMenu.enabled = true;
+        startOnlineText.enabled = false;
+        startOfflineText.enabled = false;
+    }
+
+    public void ExitPress()
     {
         quitMenu.enabled = true;
-        startHunterText.enabled = false;
-        startDeerText.enabled = false;
+        startOnlineText.enabled = false;
+        startOfflineText.enabled = false;
         exitText.enabled = false;
+
+
+    }
+
+    public void goBackPress()
+    {
+        quitMenu.enabled = false;
+        OfflineMenu.enabled = false;
+        OnlineMenu.enabled = false;
+        startOnlineText.enabled = true;
+        startOfflineText.enabled = true;
 
 
     }
@@ -36,17 +84,17 @@ public class MenuScript : MonoBehaviour {
     public void NoPress()
     {
         quitMenu.enabled = false;
-        startHunterText.enabled = true;
-        startDeerText.enabled = true;
+        startOnlineText.enabled = true;
+        startOfflineText.enabled = true;
         exitText.enabled = true;
     }
 
-    public void StartDeerLevel()
+    public void startDeerOffline()
     {
         SceneManager.LoadScene("DeerScene");
     }
 
-    public void StartHunterLevel()
+    public void startHunterOffline()
     {
         SceneManager.LoadScene("HunterScene");
     }
