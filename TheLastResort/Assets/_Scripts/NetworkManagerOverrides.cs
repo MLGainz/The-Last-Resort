14b6193d0447edb8091c3a07dde6ad8499b1792d
Sean Leonard <spleonard1@yahoo.com>
2017-11-09 23:49:23 -0500

    Done for the night, working on multiple prefabs for multiplayer

---
+++ TheLastResort/Assets/_Scripts/NetworkManagerOverrides.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManagerOverrides : NetworkManager {
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId){
+
+	}
+}
