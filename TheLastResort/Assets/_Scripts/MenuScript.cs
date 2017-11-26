742d9a07c27860b612545d1fee531603586f8773
dfowler621 <dfowler621@gmail.com>
2017-11-26 13:57:39 -0500

    reimplemented start menu

--- TheLastResort/Assets/_Scripts/MenuScript.cs
+++ TheLastResort/Assets/_Scripts/MenuScript.cs
@@ -7,32 +7,29 @@ using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour {
 
     public Canvas quitMenu;
-    public Canvas OfflineMenu;
-    public Canvas OnlineMenu;
-    public Button startHunterText;
-    public Button startDeerText;
+   
+    
+   
+    
     public Button exitText;
     public Button startOnlineText;
-    public Button startOfflineText;
-    public Button GoBackText;
-    public GameObject hunter;
-    public GameObject deer;
-    public GameObject MenuStuff;
+    
+    
 
 	// Use this for initialization
 	void Start () {
 
         quitMenu = quitMenu.GetComponent<Canvas>();
         startOnlineText = startOnlineText.GetComponent<Button>();
-        startOfflineText = startOfflineText.GetComponent<Button>();
+        
 
         //startHunterText = startHunterText.GetComponent<Button>();
         //startDeerText = startDeerText.GetComponent<Button>();
         exitText = exitText.GetComponent<Button>();
         //startHunterText.enabled = false;
         //startDeerText.enabled = false;
-        OfflineMenu.enabled = false;
-        OnlineMenu.enabled = false;
+       
+        
         quitMenu.enabled = false;
 
 		
@@ -42,23 +39,19 @@ public class MenuScript : MonoBehaviour {
 
     public void OnlinePress()
     {
-        OnlineMenu.enabled = true;
+       
         startOnlineText.enabled = false;
-        startOfflineText.enabled = false;
+        startOnlineLobby();
+        
     }
 
-    public void OfflinePress()
-    {
-        OfflineMenu.enabled = true;
-        startOnlineText.enabled = false;
-        startOfflineText.enabled = false;
-    }
+    
 
     public void ExitPress()
     {
         quitMenu.enabled = true;
         startOnlineText.enabled = false;
-        startOfflineText.enabled = false;
+       
         exitText.enabled = false;
 
 
@@ -67,10 +60,10 @@ public class MenuScript : MonoBehaviour {
     public void goBackPress()
     {
         quitMenu.enabled = false;
-        OfflineMenu.enabled = false;
-        OnlineMenu.enabled = false;
+        
+       
         startOnlineText.enabled = true;
-        startOfflineText.enabled = true;
+       
 
 
     }
@@ -80,47 +73,18 @@ public class MenuScript : MonoBehaviour {
     {
         quitMenu.enabled = false;
         startOnlineText.enabled = true;
-        startOfflineText.enabled = true;
+       
         exitText.enabled = true;
     }
 
-    public void startDeerOffline()
+   
+
+    public void startOnlineLobby()
     {
-        print("Start as Deer offline");
-
-        MenuStuff.SetActive(false);
-        GameObject go = (GameObject)Instantiate(deer);
-
-        //add AI stuff here for AI hunter
+        SceneManager.LoadScene("Lobby");
     }
 
-    public void startHunterOffline()
-    {
-        print("Start as Hunter offline");
-        MenuStuff.SetActive(false);
-        GameObject go = (GameObject)Instantiate(hunter);
-
-        //add AI stuff here for AI deer
-
-    }
-
-    public void startHunterOnline()
-    {
-        print("Start as Hunter online");
-        MenuStuff.SetActive(false);
-        GameObject go = (GameObject)Instantiate(hunter);
-
-        //add online stuff here
-    }
-
-    public void startDeerOnline()
-    {
-        print("Start as Deer online");
-        MenuStuff.SetActive(false);
-        GameObject go = (GameObject)Instantiate(deer);
-
-        //add online stuff here
-    }
+   
 
     public void ExitGame()
     {
