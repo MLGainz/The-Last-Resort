c660f103c06a17409b04278eb6a2d98ea76b5098
dfowler621 <dfowler621@gmail.com>
2017-11-30 18:51:11 -0500

    Revert "Stuff"
    This reverts commit 8a5f90d7d8a46c3b3398e2bd13e8c055bae86fda.

--- TheLastResort/Assets/_Scripts/BowScript.cs
+++ TheLastResort/Assets/_Scripts/BowScript.cs
@@ -51,9 +51,12 @@ public class BowScript : NetworkBehaviour {
 			if (Input.GetMouseButtonDown (0)) {
 				isDrawn = true;
 <<<<<<< HEAD
+<<<<<<< HEAD
 				//fakeArrow.SetActive (true);
 				fakeProjectile = Instantiate (prefabFakeProjectile, launchPos, launchRot);
 =======
+=======
+>>>>>>> parent of 8a5f90d7... Stuff
 				fakeArrow.SetActive (true);
 				fakeArrow.transform.position = launchPos;
 				fakeArrow.transform.rotation = launchRot;
@@ -66,10 +69,15 @@ public class BowScript : NetworkBehaviour {
 					nextShot = Time.time + coolDown;
 					canShoot = false;
 <<<<<<< HEAD
+<<<<<<< HEAD
 					fakeProjectile.GetComponent<Rigidbody> ().isKinematic = false;
 					fakeProjectile.GetComponent<Rigidbody> ().velocity = fakeProjectile.transform.forward * power * timeHeld;
 					//fakeArrow.SetActive (false);
 					//CmdShootArrow (launchPos, launchRot, power, timeHeld);
+=======
+					fakeArrow.SetActive (false);
+					CmdShootArrow (launchPos, launchRot, power, timeHeld);
+>>>>>>> parent of 8a5f90d7... Stuff
 =======
 					fakeArrow.SetActive (false);
 					CmdShootArrow (launchPos, launchRot, power, timeHeld);
